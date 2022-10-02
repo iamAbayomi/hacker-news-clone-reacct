712eb256325faa9fd0fa400329bbf321021fc932
iamAbayomi <oladinifolawiyo@gmail.com>
2022-10-02 22:36:16 +0100

    feat: added footer

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,20 @@
 /* eslint-disable @next/next/no-img-element */
 import Headline from "@/components/headline";
 import { useGetNews, useGetNewsItem } from "@/hooks/story";
-import { Box, Text } from "@chakra-ui/react";
+import { Box, Input, Text } from "@chakra-ui/react";
 import { INewsItem } from "../types";
 
 function Index() {
-  // const { news, isLoading } = useGetNews();
-  // console.log("news", news);
-  const { newsItem, isLoading } = useGetNewsItem("33054212");
+  const { newsItem, isLoading } = useGetNewsItem();
+  const footerLists = [
+    "Guidelines",
+    "FAQ",
+    "Lists",
+    "API",
+    "Security",
+    "Legal",
+    "Apply to YC"
+  ];
   console.log("newsItem", newsItem, "items");
   return (
     <div className="App">
@@ -36,23 +43,64 @@ function Index() {
             <p>login</p>
           </div>
         </div>
-
         <div className="lists-of-headline">
           {isLoading ? (
             <Text>isLoading...</Text>
           ) : (
             <Box>
               {newsItem?.map((item: INewsItem, index: number) => (
-                <Headline key={index} headline={item} />
+                <Headline key={index} newsId={++index} headline={item} />
               ))}
             </Box>
           )}
-          {/* {isLoading ? (
-            <Text>isLoading...</Text>
-          ) : (
-            <Headline headline={newsItem} />
-          )} */}
         </div>
+        <Text m={"10px 0px 20px 50px"} color={"#828282"}>
+          More
+        </Text>
+        <Box borderTop={"2px solid #ff6600"} />
+        <Box maxWidth={"469px"} margin={"30px auto 10px"} pb={"20px"}>
+          <Text fontSize={"15px"} textAlign={"center"} fontWeight={"400"}>
+            Applications are open for YC Winter 2023
+          </Text>
+          <Box display={"flex"} margin={"auto"} alignItems={"center"}>
+            {footerLists.map((item: string, index: number) => (
+              <Box
+                display={"flex"}
+                alignItems={"center"}
+                key={index}
+                // border={"1px solid"}
+              >
+                <Text fontSize={"11px"} margin={"10px"}>
+                  {item}
+                </Text>
+                <Box borderRight={"1px solid"} height={"13px"} />
+              </Box>
+            ))}
+            <Text margin={"10px"} fontSize={"11px"}>
+              Contact
+            </Text>
+          </Box>
+          <Box
+            maxWidth={"200px"}
+            margin={"5px auto"}
+            display={"flex"}
+            alignItems={"center"}
+          >
+            <Text fontSize={"15px"} color={"#828282"} fontWeight={"500"}>
+              Search:{" "}
+            </Text>
+            <Input
+              type={"name"}
+              height={"21.5px"}
+              width={"148px"}
+              ml={"5px"}
+              background={"white"}
+              borderRadius={"1px"}
+              border={"1px solid black"}
+              borderColor={"black"}
+            />
+          </Box>
+        </Box>
       </div>
     </div>
   );
