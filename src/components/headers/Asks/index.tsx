e959d5d4ff925a0ba7ade89cbda8f55d371cd1a7
iamAbayomi <oladinifolawiyo@gmail.com>
2022-10-18 21:13:44 +0100

    feat: add headers component

---
+++ src/components/headers/Asks/index.tsx
@@ -0,0 +1,7 @@
+import { Box } from "@chakra-ui/react";
+
+const Asks = () => {
+  return <Box></Box>;
+};
+
+export default Asks;
---
+++ src/components/headers/Jobs/index.tsx
@@ -0,0 +1,11 @@
+import { Box } from "@chakra-ui/react";
+
+const Jobs = () => {
+  return (
+    <Box>
+      <p>s</p>
+    </Box>
+  );
+};
+
+export default Jobs;
